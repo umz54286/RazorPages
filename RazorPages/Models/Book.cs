c46524e3d025fa9eadd8e5f04bc664195fe6fb71
Yufang <umz54286@gmail.com>
2024-07-25 16:25:12 +0800

    Book Model add Validation

--- RazorPages/Models/Book.cs
+++ RazorPages/Models/Book.cs
@@ -5,12 +5,23 @@ namespace RazorPages.Models
     public class Book
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
         public string ProductNo { get; set; }
 
+        [Required]
+        [StringLength(200, MinimumLength = 3)]
         public string ProductName { get; set; }
 
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Type { get; set; }
 
+        [Required]
+        [Range(1, 1000000)]
+        [DataType(DataType.Currency)]
         public int Price { get; set; }
         
         [DataType(DataType.Date)]
